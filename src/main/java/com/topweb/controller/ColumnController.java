31256ced8a7999da111cc45ea45417ffaeeb39fd
wushoulei <wushoulei@uban.com>
2017-09-27 18:45:35 +0800

    栏目模块开发

--- src/main/java/com/topweb/controller/ColumnController.java
+++ src/main/java/com/topweb/controller/ColumnController.java
@@ -1,7 +1,16 @@
 package com.topweb.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by Leo on 2017/9/21.
  */
 public class ColumnController {
+
+    @RequestMapping("columns.html")
+    public ModelAndView columnsSetting() {
+        ModelAndView view = new ModelAndView("");
+        return view;
+    }
 }
