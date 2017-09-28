51b6bc3cfe83e4b67d9fafc6b331cd47b339ba1c
wushoulei <wushoulei@uban.com>
2017-09-28 17:30:52 +0800

    栏目列表页开发

--- src/main/java/com/topweb/controller/ColumnController.java
+++ src/main/java/com/topweb/controller/ColumnController.java
@@ -1,16 +1,18 @@
 package com.topweb.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Leo on 2017/9/21.
  */
+@Controller
 public class ColumnController {
 
     @RequestMapping("columns.html")
     public ModelAndView columnsSetting() {
-        ModelAndView view = new ModelAndView("");
+        ModelAndView view = new ModelAndView("column_list");
         return view;
     }
 }
